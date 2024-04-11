1133f83ef9b41ef3edd75accf274439a12c7ab66
VersionMaruti <156605831+VersionMaruti@users.noreply.github.com>
2024-04-11 15:31:38 +0530

    Add ProxyUrl

--- js/script.js
+++ js/script.js
@@ -14,9 +14,10 @@ function secondsToMinutesSeconds(seconds) {
     return `${formattedMinutes}:${formattedSeconds}`;
 }
 
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 async function getSongs(folder) {
     currfolder=folder
-    let a = await fetch(`https://github.com/VersionMaruti/Spotify-Clone/tree/main/songs/${folder}/`);
+    let a = await fetch(proxyUrl + `https://github.com/VersionMaruti/Spotify-Clone/tree/main/songs/${folder}/`);
     let response = await a.text();
     let htmlContent = response
     // console.log(response)
@@ -70,7 +71,7 @@ const PlayMusic = (track,pause=false)=>{
 }
 
 async function displayAlbums(){
-    let a = await fetch(`https://github.com/VersionMaruti/Spotify-Clone/tree/main/songs`);
+    let a = await fetch(proxyUrl + `https://github.com/VersionMaruti/Spotify-Clone/tree/main/songs`);
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response
@@ -81,7 +82,7 @@ async function displayAlbums(){
 
         if(e.href.includes("/songs/")){//**/
              let folder = e.href.split("/").slice(-1)[0]
-             let a = await fetch(`https://github.com/VersionMaruti/Spotify-Clone/tree/main/songs/${folder}/info.json`)
+             let a = await fetch(proxyUrl + `https://github.com/VersionMaruti/Spotify-Clone/tree/main/songs/${folder}/info.json`)
              let response = await a.json();
             //  console.log(response)
              let cardContainer = document.querySelector(".card-container")
