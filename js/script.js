aeb10e1fc803b297dc819efc6985f84f67c0f82d
VersionMaruti <156605831+VersionMaruti@users.noreply.github.com>
2024-04-11 13:10:48 +0530

    Update paths

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currfolder=folder
-    let a = await fetch(`/${folder}/`)
+    let a = await fetch(`https://versionmaruti.github.io/${folder}/`);
     let response = await a.text();
     let htmlContent = response
     // console.log(response)
@@ -59,7 +59,7 @@ async function getSongs(folder) {
 }
 let currentSong = new Audio();
 const PlayMusic = (track,pause=false)=>{
-    currentSong.src = `/${currfolder}/` + track;
+    currentSong.src = `https://versionmaruti.github.io/${currfolder}/` + track;
     if(!pause){
         currentSong.play()
         play.src = "img/pause.svg"
@@ -70,7 +70,7 @@ const PlayMusic = (track,pause=false)=>{
 }
 
 async function displayAlbums(){
-    let a = await fetch(`/songs/`)
+    let a = await fetch(`https://versionmaruti.github.io/songs/`);
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response
@@ -81,7 +81,7 @@ async function displayAlbums(){
 
         if(e.href.includes("/songs/")){//**/
              let folder = e.href.split("/").slice(-1)[0]
-             let a = await fetch(`/songs/${folder}/info.json`)
+             let a = await fetch(`https://versionmaruti.github.io/songs/${folder}/info.json`)
              let response = await a.json();
             //  console.log(response)
              let cardContainer = document.querySelector(".card-container")
