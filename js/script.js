460e2677cdfcfb873ae323cb4cfc29af9d794590
VersionMaruti <156605831+VersionMaruti@users.noreply.github.com>
2024-04-11 16:50:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,10 +14,9 @@ function secondsToMinutesSeconds(seconds) {
     return `${formattedMinutes}:${formattedSeconds}`;
 }
 
-const proxyUrl = 'https://0.0.0.0:8080/';
 async function getSongs(folder) {
     currfolder=folder
-    let a = await fetch(proxyUrl + `https://github.com/VersionMaruti/Spotify-Clone/tree/main/songs/${folder}/`);
+    let a = await fetch(`/Video%2084/${folder}/`)
     let response = await a.text();
     let htmlContent = response
     // console.log(response)
@@ -60,7 +59,7 @@ async function getSongs(folder) {
 }
 let currentSong = new Audio();
 const PlayMusic = (track,pause=false)=>{
-    currentSong.src = `https://github.com/VersionMaruti/Spotify-Clone/tree/main/songs/${currfolder}/` + track;
+    currentSong.src = `/Video%2084/${currfolder}/` + track;
     if(!pause){
         currentSong.play()
         play.src = "img/pause.svg"
@@ -71,7 +70,7 @@ const PlayMusic = (track,pause=false)=>{
 }
 
 async function displayAlbums(){
-    let a = await fetch(proxyUrl + `https://github.com/VersionMaruti/Spotify-Clone/tree/main/songs`);
+    let a = await fetch(`/Video%2084/songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response
@@ -82,7 +81,7 @@ async function displayAlbums(){
 
         if(e.href.includes("/songs/")){//**/
              let folder = e.href.split("/").slice(-1)[0]
-             let a = await fetch(proxyUrl + `https://github.com/VersionMaruti/Spotify-Clone/tree/main/songs/${folder}/info.json`)
+             let a = await fetch(`/Video%2084/songs/${folder}/info.json`)
              let response = await a.json();
             //  console.log(response)
              let cardContainer = document.querySelector(".card-container")
